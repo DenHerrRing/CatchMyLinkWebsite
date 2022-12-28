d4fea3b12bf96e0f0dc2d48e769dc53e34bdcbf4
Dennis Hering <dennis@hering.me>
2022-12-28 12:23:06 +0100

    fix social add modal

--- src/app/pages/dashboard/links/add-social-modal/add-social-modal.component.ts
+++ src/app/pages/dashboard/links/add-social-modal/add-social-modal.component.ts
@@ -2,6 +2,8 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Social} from "../../../../shared/models/social";
 import {FormsModule} from "@angular/forms";
+import {IconService} from "../../../../shared/services/icon.service";
+import {SocialIcon} from "../../../../shared/models/social-icon";
 
 @Component({
     selector: 'app-add-social-modal',
@@ -15,14 +17,38 @@ export class AddSocialModalComponent {
     @Output() closeModal: EventEmitter<boolean> = new EventEmitter<boolean>()
     @Output() save: EventEmitter<Social> = new EventEmitter<Social>()
 
+    showDetails: boolean = false
+
     newSocial: Social = new Social()
 
-    constructor() {
+    constructor(public socialIcons: IconService) {
+        console.log(socialIcons.getSocialIcons())
+    }
+
+    onClickSelectIcon(icon: SocialIcon): void {
+        this.newSocial.iconUrl = icon.iconUrl
+        this.newSocial.iconColor = icon.iconColor
+        this.newSocial.name = icon.name
+        this.newSocial.preUrl = icon.preUrl
+        this.newSocial.placeholder = icon.placeholder
+        this.onClickNext()
     }
 
     onClickCloseModal(): void {
         this.newSocial = new Social()
+        this.showDetails = false
+        this.showModal = false
         this.closeModal.emit(false)
+    }
+
+    onClickNext(): void {
+        this.showModal = false
+        this.showDetails = true
+    }
+
+    onClickBack(): void {
+        this.showModal = true
+        this.showDetails = false
     }
 
     onClickSave(social: Social): void {
