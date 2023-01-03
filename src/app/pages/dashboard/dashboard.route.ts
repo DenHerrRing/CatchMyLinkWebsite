1442fb97ca13c0e940529db5c3cf86f4a1ede686
Dennis Hering <dennis@hering.me>
2023-01-03 08:34:42 +0100

    Add Routing for dashboard.component.html

---
+++ src/app/pages/dashboard/dashboard.route.ts
@@ -0,0 +1,12 @@
+import {Route} from "@angular/router";
+import {LinksComponent} from "./links/links.component";
+import {DesignComponent} from "./design/design.component";
+import {StatsComponent} from "./stats/stats.component";
+import {ConfigsComponent} from "./configs/configs.component";
+
+export const DASHBOARD_ROUTES: Route[] = [
+    { path: 'links', component: LinksComponent },
+    { path: 'design', component: DesignComponent },
+    { path: 'stats', component: StatsComponent },
+    { path: 'config', component: ConfigsComponent },
+]
